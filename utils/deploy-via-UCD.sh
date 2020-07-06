78a729e3e19bcc02cc51321ca6367fab65520987
Dalwinder Bagdi <dalwinder.bagdi@ibm.com>
2020-07-06 17:36:05 +0100

    Fixing deploy to UCD script

--- utils/deploy-via-UCD.sh
+++ utils/deploy-via-UCD.sh
@@ -1,33 +1,47 @@
 #!/bin/bash
-
 printf "\n
 ----------------------------------------------------------------------------------------------------------------------------------------
 \n 1. Creating snapshot \n
 ----------------------------------------------------------------------------------------------------------------------------------------\n"
+
 printf "\n ./snapshot.json contents:\n"
-echo '{ "name": "2.0", "application": "ace-custom", "description": "from tekton pipeline", "versions": [ {"ace-custom-image": "latest"} ]}' | tee ./snapshot.json
+printf "{
+  \"name\": \"%s\",
+  \"application\": \"%s\",
+  \"description\": \"from tekton pipeline\",
+  \"versions\": [{
+    \"%s\": \"%s\"
+  }]
+}" "${SNAPSHOT_ID}" "${APPLICATION_NAME}" "${SNAPSHOT_VERSION_NAME}" "${SNAPSHOT_VERSION}" | tee ./snapshot.json
 
 printf "\n"
-
-curl -k -u "$UCD_USER":"$UCD_PASSWORD" "$UCD_URL:$UCD_PORT/cli/snapshot/createSnapshot" -X PUT --data @snapshot.json
+curl -k -u "${UCD_USER}":"${UCD_PASSWORD}" "${UCD_URL}:${UCD_PORT}/cli/snapshot/createSnapshot" -X PUT --data @snapshot.json
 
 printf "\n \n \n
 ----------------------------------------------------------------------------------------------------------------------------------------
 \n 2. Creating json file with application json \n
 ----------------------------------------------------------------------------------------------------------------------------------------\n"
-echo '{"application": "ace-custom", "applicationProcess": "deploy", "environment": "DEV", "onlyChanged": "false", "snapshot":"2.0" }' | tee ./deployment.json
+
+printf "{
+  \"application\": \"%s\",
+  \"applicationProcess\": \"%s\",
+  \"environment\": \"%s\",
+  \"onlyChanged\": \"false\",
+  \"snapshot\": \"%s\"
+}" "${APPLICATION_NAME}" "${APPLICATION_PROCESS}" "${ENV}" "${SNAPSHOT_ID}" | tee ./deployment.json
 
 printf "\n \n
 ----------------------------------------------------------------------------------------------------------------------------------------
 \n 3. Requesting deployment from snapshot \n
 ----------------------------------------------------------------------------------------------------------------------------------------\n"
-printf "Output from 'curl -k -u %s:%s %s:%s/cli/applicationProcessRequest/request -X PUT -d @deployment.json': ", "$UCD_USER" "$UCD_PASSWORD" "$UCD_URL" "$UCD_PORT"
+printf "Output from 'curl -k -u %s:%s %s:%s/cli/applicationProcessRequest/request -X PUT -d @deployment.json': ", "${UCD_USER}" "${UCD_PASSWORD}" "${UCD_URL}" "${UCD_PORT}"
 
-REQUEST=$(
-  curl -k -u "$UCD_USER":"$UCD_PASSWORD"
-  "$UCD_URL:$UCD_PORT/cli/applicationProcessRequest/request" -X PUT -d @deployment.json
-)
+REQUEST=$(curl -k -u "${UCD_USER}":"${UCD_PASSWORD}" "${UCD_URL}":"${UCD_PORT}"/cli/applicationProcessRequest/request -X PUT -d @deployment.json)
 
 REQUEST_ID="${REQUEST:14:36}"
 
-echo "Waiting for request to complete"
+printf "Requesting status for request ID %s until request has been completed\n", "${REQUEST_ID}"
+
+until curl -k -u "${UCD_USER}":"${UCD_PASSWORD}" "${UCD_URL}":"${UCD_PORT}"/cli/applicationProcessRequest/requestStatus?request=${REQUEST_ID} | grep -e CLOSED -e FAULTED;
+do sleep 5;
+done;
