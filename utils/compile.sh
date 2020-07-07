b8b11d33d5763ae26e6f4cf1697fb95d9185012d
Dalwinder Bagdi <dalwinder.bagdi@ibm.com>
2020-07-07 09:09:55 +0100

    Adding trace on mqscicreatebar

--- utils/compile.sh
+++ utils/compile.sh
@@ -7,6 +7,6 @@ export DISPLAY=:100
 cd workspace
 echo "Generating the BAR file in the location" ./workspace/${PROJECT}/gen/${PROJECT}.bar
 
-mqsicreatebar -data . -b ./${PROJECT}/gen/${PROJECT}.bar -a ${PROJECT}
+mqsicreatebar -data . -b ./${PROJECT}/gen/${PROJECT}.bar -a ${PROJECT} -trace
 
 echo Compile completed
